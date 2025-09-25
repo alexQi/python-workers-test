7f3d871064e54f6193ff244659bb7676f67440cc
alex <alex.qiubo@qq.com>
2025-09-25 17:30:40 +0800

    Update entry.py

--- 01-hello/src/entry.py
+++ 01-hello/src/entry.py
@@ -1,6 +1,10 @@
 from workers import WorkerEntrypoint, Response
-
+import requests
+from requests.models import Response
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request, env):
-        return Response("Hello world!")
+        url = 'https://upbit.com/'
+        resp: Response = requests.get(url, stream=True)
+        remote_address = resp.raw._connection.sock.getpeername()
+        return Response(remote_address)
